0d4e7ff2e75b0bc6df6a457175d2b24c633aee86
almazesh <meanwhilealmaz@gmail.com>
2021-08-10 03:16:09 +0600

    add the pagination

--- js/script.js
+++ js/script.js
@@ -11,7 +11,11 @@ for(var i =0 ; i < 20 ;i++){
 // Random color
 
 
+var offset = 0
+var limit = 20
+var count = 1
 
+var state = 1118 / 20
 
 const container = document.querySelector('.row')
 
@@ -28,7 +32,7 @@ function getDate(url ,query , cb){
 
 
 window.addEventListener('load' , () =>{
-    getDate(base , 'offset=0&limit=20' , res =>{
+    getDate(base , `offset=${offset}&limit=${limit}` , res =>{
         const temp = res.results.map(item =>{
             return`
                 <div class="col-xl-4 mt-5 ">
@@ -71,3 +75,123 @@ function getSingle(url){
 
     })
 }
+
+
+const next = document.querySelector('.next')
+var countRes = document.querySelector('.count')
+
+next.addEventListener('click' , e=> {
+    e.preventDefault();
+
+    getDate(base ,`offset=${offset}&limit=${limit}` , res =>{
+        if(state > res.count){
+            container.innerHTML = `Empty`
+        }else{
+            const temp = res.results.map(item =>{
+                return`
+                <div class="col-xl-4 mt-5 ">
+                    <div class="card" style="background:${hex};cursor:pointer" onclick="getSingle('${item.url}')">
+                        <div class="card-header text-center text-light " style="text-transform:capitalize">
+                            <h5>${item.name}</h5>
+                        </div>
+                    </div>
+                </div>
+            `
+        }).join('')
+
+        container.innerHTML = temp
+        }
+    })
+
+    offset+= 20
+    count++
+
+    countRes.innerHTML = count
+
+    if(count > state){
+        next.classList.add('disabled')
+    }
+
+
+    if(count > 1){
+        prev.classList.remove('disabled')
+    }
+
+    
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+const prev = document.querySelector('.prev');
+
+prev.addEventListener('click' , e=>{
+    e.preventDefault();
+
+    getDate(base ,`offset=${offset}&limit=${limit}` , res =>{
+        const temp = res.results.map(item =>{
+                return`
+                <div class="col-xl-4 mt-5 ">
+                    <div class="card" style="background:${hex};cursor:pointer" onclick="getSingle('${item.url}')">
+                        <div class="card-header text-center text-light " style="text-transform:capitalize">
+                            <h5>${item.name}</h5>
+                        </div>
+                    </div>
+                </div>
+            `
+        }).join('')
+
+        container.innerHTML = temp
+    })
+    offset-= 20
+    count--
+
+    countRes.innerHTML = count
+
+    if(count < 1){
+        prev.classList.add('disabled')
+    }
+
+    if(count < state){
+        next.classList.remove('disabled')
+    }
+
+})
+
+
+
+
+const search = document.querySelector('.search')
+
+
+search.addEventListener('input' , e=>{
+    var val = e.target.value.toUpperCase();
+
+    getDate(base , '' , res =>{
+        const find = res.results.map((item) => {
+            if(item.name.toUpperCase().includes(val)){
+                return`
+                    <div class="col-xl-4 mt-5 ">
+                        <div class="card" style="background:${hex};cursor:pointer" onclick="getSingle('${item.url}')">
+                            <div class="card-header text-center text-light " style="text-transform:capitalize">
+                                <h5>${item.name}</h5>
+                            </div>
+                        </div>
+                    </div>
+                `
+            }
+        }).join('')
+
+        container.innerHTML = find
+
+    })
+})
